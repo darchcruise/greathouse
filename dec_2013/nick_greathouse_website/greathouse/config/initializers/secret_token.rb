fa75dd105d363ab31dd26f98f950cb1314c07782
darchcruise <darchcruise@yahoo.com>
2013-12-17 15:07:07 -0500

    added twitter news feed to rails app via view page

---
+++ dec_2013/nick_greathouse_website/greathouse/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Greathouse::Application.config.secret_token = '98f3f6a9350f34f6ae6998160a9d3fc5ea4f9150052fc5c094361628c0f8b3c865d057c317066b342380b1e7a909b81714ad01d62a12c8e239834a0ce9497d57'
